612b3313f241572604695cd3a0e07c5cc3230f3f
Karolina Viragova <karolinavi94@gmail.com>
2025-11-14 14:33:39 +0100

    take a tour component

---
+++ src/components/TakeATour.jsx
@@ -0,0 +1,6 @@
+
+export const TakeATour = () => {
+  return (
+    <p>text</p>
+  );
+};
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,10 +1,12 @@
 import { ButtonsContainer } from '@/components/ButtonsContainer';
+import { TakeATour } from '@/components/TakeATour';
 
 export const Homepage = () => {
   return (
     <>
       <div className='h-screen overflow-y-auto'>
-        <ButtonsContainer />
+        <ButtonsContainer />  
+        <TakeATour />      
         <p>Challo</p>
       </div>
     </>
