783b00ade8d58ea1df804d3935b395490c5f0795
Eva Nunez <evanunez@Eva--MacBook-Air.local>
2025-11-14 14:45:48 +0100

    implement carousel, styling to takeATour component

--- src/components/TakeATour.jsx
+++ src/components/TakeATour.jsx
@@ -1,6 +1,29 @@
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel"
+
 
 export const TakeATour = () => {
   return (
-    <p>text</p>
+    <>
+      <div className="w-full max-w-xs mx-auto">
+        <Carousel>
+          <CarouselContent>
+            <CarouselItem> <div className="p-4 bg-gray-200 rounded">Slide 1</div> </CarouselItem>
+            <CarouselItem> <div className="p-4 bg-gray-200 rounded">Slide 2</div> </CarouselItem>
+            <CarouselItem> <div className="p-4 bg-gray-200 rounded">Slide 3</div> </CarouselItem>
+          </CarouselContent>
+
+          <CarouselPrevious />
+          <CarouselNext />
+        </Carousel>
+      </div>
+      <div className='className="max-w-prose mx-auto px-10 mt-8 space-y-2 text-gray-700 leading-loose"'><p>Our gym is the best gym</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur eum adipisci inventore delectus sit alias fugiat vero assumenda facilis dolorem commodi blanditiis reiciendis minima, quaerat atque quidem. Ducimus, esse expedita.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis temporibus odit perspiciatis in eum fugiat cupiditate voluptatibus minus ut repudiandae aliquid, sit doloremque deserunt optio, quos reprehenderit? Labore, fugit iusto!</p></div></>
   );
 };
