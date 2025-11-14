1e8d24d8e21a6dea23ff5b9e7ebfbeb6d9a252b8
Eva Nunez <evanunez@Eva--MacBook-Air.local>
2025-11-14 15:13:07 +0100

    format Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,10 @@
+import logo from '../images/logo-placeholder-image.png';
+
 export const Header = () => {
   return (
-    <>
-      <header>This is header</header>
-    </>
+    <header className='sticky top-0 z-50 flex items-center justify-center gap-3 py-2 bg-gray-700 shadow-sm'>
+      <img src={logo} alt="logo" className='h-24 w-24 object-contain' />
+      <p className='text-2xl font-semibold text-gray-50'>VaultFit</p>
+    </header>
   );
 };
