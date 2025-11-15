7389677118d47c0476b62d1458b623a2c0d1cb5b
Eva Nunez <evanunez@Mac.localdomain>
2025-11-15 15:32:13 +0100

    update header colors

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ export const Header = () => {
     <header className="sticky top-0 z-50 flex items-center flex-col gap-3 py-2 bg-background-light md:flex-row md:justify-evenly">
       <div className='flex flex-col items-center md:flex-row'>
         <img src={logo} alt="logo" className="h-24 w-24 object-contain" />
-        <p className="text-2xl font-semibold text-gray-50">VaultFit</p>
+        <p className="text-2xl font-semibold text-gold-light">VaultFit</p>
       </div>
       <ButtonsContainer />
     </header>
