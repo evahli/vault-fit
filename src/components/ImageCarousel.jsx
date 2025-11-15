03c68202a0c648f30dc30fbd0ab50ccf8056a91e
Karolina Viragova <karolinavi94@gmail.com>
2025-11-15 12:13:36 +0100

    carousel image

---
+++ src/components/ImageCarousel.jsx
@@ -0,0 +1,64 @@
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from '@/components/ui/carousel';
+
+import placeholder from '../images/placeholder-image-carousel.jpg';
+
+export const ImageCarousel = () => {
+  return (
+    <>
+      <div className='w-screen'>
+        <Carousel className="w-full max-w-xs mx-auto">
+          <CarouselContent>
+            <CarouselItem>
+              <img src={placeholder} alt="placeholder"/>
+              <p className="text-center pt-4">Description of first image</p>
+            </CarouselItem>
+            <CarouselItem>
+              <img src={placeholder} alt="placeholder" />
+              <p className="text-center pt-4">Description of second image</p>
+            </CarouselItem>
+            <CarouselItem>
+              <img src={placeholder} alt="placeholder" />
+              <p className="text-center pt-4">Description of third image</p>
+            </CarouselItem>
+            <CarouselItem>
+              <img src={placeholder} alt="placeholder" />
+              <p className="text-center pt-4">Description of fourth image</p>
+            </CarouselItem>
+            <CarouselItem>
+              <img src={placeholder} alt="placeholder" />
+              <p className="text-center pt-4">Description of fifth image</p>
+            </CarouselItem>
+            <CarouselItem>
+              <img src={placeholder} alt="placeholder" />
+              <p className="text-center pt-4">Description of sixth image</p>
+            </CarouselItem>
+          </CarouselContent>
+
+          <CarouselPrevious/>
+          <CarouselNext/>
+        </Carousel>
+      </div>
+      <div className='className="max-w-prose mx-auto px-10 mt-8 space-y-2 text--text leading-loose"'>
+        <p>Our gym is the best gym</p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur
+          eum adipisci inventore delectus sit alias fugiat vero assumenda
+          facilis dolorem commodi blanditiis reiciendis minima, quaerat atque
+          quidem. Ducimus, esse expedita.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis
+          temporibus odit perspiciatis in eum fugiat cupiditate voluptatibus
+          minus ut repudiandae aliquid, sit doloremque deserunt optio, quos
+          reprehenderit? Labore, fugit iusto!
+        </p>
+      </div>
+    </>
+  );
+};
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,13 +1,14 @@
 import { ButtonsContainer } from '@/components/ButtonsContainer';
+import { ImageCarousel } from '@/components/imageCarousel';
 import { InfoCards } from '@/components/InfoCards';
-import { TakeATour } from '@/components/TakeATour';
+
 
 export const Homepage = () => {
   return (
     <>
       <div className='h-screen overflow-y-auto'>
         <ButtonsContainer />  
-        <TakeATour />
+        <ImageCarousel />
         <InfoCards />
       </div>
     </>
