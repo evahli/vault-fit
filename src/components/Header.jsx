c036ba601684318a1357aae98af7697801bc8dc6
Karolina Viragova <karolinavi94@gmail.com>
2025-11-15 12:47:42 +0100

    header fix

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import logo from '../images/logo-placeholder-image.png';
 
 export const Header = () => {
   return (
-    <header className='sticky top-0 z-50 flex items-center justify-center gap-3 py-2 bg-vaultfit-background-light border-b border-vaultfit-bordershadow-sm'>
+    <header className='sticky top-0 z-50 flex items-center justify-center gap-3 py-2 bg-background-light border-b border-vaultfit-bordershadow-sm'>
       <img src={logo} alt="logo" className='h-24 w-24 object-contain' />
       <p className='text-2xl font-semibold text-gray-50'>VaultFit</p>
     </header>
